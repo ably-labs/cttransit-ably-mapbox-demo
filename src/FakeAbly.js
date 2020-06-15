9bf32ec2901f42abcb534bf67ebd0fd8a1f57516
thisisjofrank <jofranchetti@gmail.com>
2020-06-15 18:56:24 +0100

    fix test data to match new data structure

--- src/FakeAbly.js
+++ src/FakeAbly.js
@@ -1,14 +1,14 @@
 const starterFakeMessage = { 
     "name":"data",
     "id":"IarXNoZzs8:0:0",
-    "data": {
+    "data": [{
         "id":"1430",
         "vehicle": {
             "position": { "latitude": 41.767179, "longitude": -72.658395, "bearing":90, "speed":0.0000034520621738920454 },
             "timestamp":"1588850403",
             "vehicle": { "id":"2430", "label": "1430" }
         }
-    }
+    }]
 };
 
 export default function simulateAblyMessages(callback) {
